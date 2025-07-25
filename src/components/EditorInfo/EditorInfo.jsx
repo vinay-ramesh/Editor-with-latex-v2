efdc6d4482e01ae0cf37b7d86a5f3575136f0aee
Vinay RS <vinay.ramesh@appsndevices.in>
2025-07-25 15:20:04 +0530

    EditorInfo created with dummy list

--- src/components/EditorInfo/EditorInfo.jsx
+++ src/components/EditorInfo/EditorInfo.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const EditorInfo = () => {
-  return (
-    <div>EditorInfo</div>
-  )
+    return (
+        <ul style={{ textAlign: "left" }}>
+            <li>Hello</li>
+            <li>Hii</li>
+            <li>How are you</li>
+            <li>I am fine and I can win also </li>
+            <li>I will win and I won also</li>
+        </ul>
+    )
 }
 
 export default EditorInfo
